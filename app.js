eb7cef64163163abb919388d2e7ccb6b9f075a49
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-03-23 02:13:38 +0900

    add js with event listernes to change img when hovered

--- app.js
+++ app.js
@@ -1,13 +1,60 @@
 const content = document.querySelector('#content');
 
-content.addEventListener('mouseover', function(e) {
-    if (e.target.tagName === 'IMG') {
-        e.target.style.display="none";
-    }
-});
+// content.addEventListener('mouseover', function(e) {
+//     if (e.target.tagName === 'IMG') {
+//         e.target.style.display="none";
+//     }
+// });
 
-content.addEventListener('mouseout',function(e){
-    if (e.target.tagName === 'IMG') {
-        e.target.style.display="grid";
-    }
-})
+// content.addEventListener('mouseout',function(e){
+//     if (e.target.tagName === 'IMG') {
+//         e.target.style.display="grid";
+//     }
+// })
+
+const images = document.querySelectorAll('#content img');
+const mainWrap = document.querySelector('#main');
+console.log(mainWrap); //test main wrap 
+
+
+images.forEach(function(img) {
+    img.addEventListener('mouseover', function() {
+      if (img.classList.contains('one')) {
+        content.style.opacity=0.2;
+        mainWrap.classList.add("backgroundOne");
+
+      } else if (img.classList.contains('two')) {
+        content.style.opacity=0.2;
+        mainWrap.classList.add("backgroundTwo");
+      } else if (img.classList.contains('three')) {
+        content.style.opacity=0.2;
+        mainWrap.classList.add("backgroundThree");;
+      } else if (img.classList.contains('four')) {
+        content.style.opacity=0.2;
+        mainWrap.classList.add("backgroundFour");
+      } else if (img.classList.contains('five')) {
+        content.style.opacity=0.2;
+        mainWrap.classList.add("backgroundFive");
+      }
+    });
+
+    img.addEventListener('mouseout', function() {
+        if (img.classList.contains('one')) {
+          content.style.opacity=1;
+          mainWrap.classList.remove("backgroundOne");
+  
+        } else if (img.classList.contains('two')) {
+          content.style.opacity=1;
+          mainWrap.classList.remove("backgroundTwo");
+        } else if (img.classList.contains('three')) {
+          content.style.opacity=1;
+          mainWrap.classList.remove("backgroundThree");;
+        } else if (img.classList.contains('four')) {
+          content.style.opacity=1;
+          mainWrap.classList.remove("backgroundFour");
+        } else if (img.classList.contains('five')) {
+          content.style.opacity=1;
+          mainWrap.classList.remove("backgroundFive");
+        }
+      });
+  });
